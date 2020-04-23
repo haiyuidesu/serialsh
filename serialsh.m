282e7b9dd2958c28b4de7c211718a7d5e0d1bc87
Yui <yunistar@icloud.com>
2020-04-23 13:23:10 +0200

    cleaned code again..

--- serialsh.m
+++ serialsh.m
@@ -5,8 +5,10 @@
 
 pid_t pd = 0;
 int pipe_shin[2], pipe_shout[2];
+
 int attribs(int fd, int baudrate) {
   struct termios tty;
+
   memset(&tty, 0x0, sizeof(tty));
   
   if (tcgetattr(fd, &tty) != 0) {
@@ -15,17 +17,19 @@ int attribs(int fd, int baudrate) {
 
   cfsetispeed(&tty, baudrate);
   cfsetospeed(&tty, baudrate);
-  tty.c_cflag &= ~CRTSCTS;
-  tty.c_cflag |= (CLOCAL | CREAD);
-  tty.c_iflag |= IGNPAR;
-  tty.c_iflag &= ~(IXON | IXOFF | INLCR | IGNCR);
-  tty.c_oflag &= ~OPOST;
-  tty.c_cflag &= ~CSIZE;
-  tty.c_cflag |= CS8;
-  tty.c_cflag &= ~PARENB;
-  tty.c_iflag &= ~INPCK;
-  tty.c_cflag &= ~CSTOPB;
-  tty.c_iflag |= INPCK;
+
+  tty.c_cflag     &= ~CRTSCTS;
+  tty.c_cflag     |= (CLOCAL | CREAD);
+  tty.c_iflag     |= IGNPAR;
+  tty.c_iflag     &= ~(IXON | IXOFF | INLCR | IGNCR);
+  tty.c_oflag     &= ~OPOST;
+  tty.c_cflag     &= ~CSIZE;
+  tty.c_cflag     |= CS8;
+
+  tty.c_cflag     &= ~PARENB;
+  tty.c_iflag     &= ~INPCK;
+  tty.c_cflag     &= ~CSTOPB;
+  tty.c_iflag     |= INPCK;
   tty.c_cc[VTIME] = 1;
   tty.c_cc[VMIN]  = 0;
 
@@ -38,9 +42,12 @@ int attribs(int fd, int baudrate) {
 
 #define spawn_shin pipe_shin[1]
 #define main_sherr pipe_shout[1]
+
 void block_attribs(int fd, int block_now) {
   struct termios tty;
+
   memset(&tty, 0x0, sizeof(tty));
+
   if (tcgetattr(fd, &tty) != 0) {
     return;
   }
@@ -52,21 +59,30 @@ void block_attribs(int fd, int block_now) {
 }
 
 int kq = 0;
+
 void spawnproc(void) {
   if (pd == 0) {
+
     pd = fork();
 
     if (pd == 0) {
       dup2(pipe_shin[0], 0);
+
       dup2(main_sherr, 1);
+
       dup2(main_sherr, 2);
+
       char *paramList[] = { "createpty", "-q", "/dev/null", "login" };
+
       execv("/usr/bin/script", paramList);
+
       exit(0);
     }
 
     struct kevent ke;
+
     EV_SET(&ke, pd, EVFILT_PROC, EV_ADD, NOTE_EXIT, 0, NULL);
+
     kevent(kq, &ke, 1, NULL, 0, NULL);
   }
 }
@@ -75,34 +91,47 @@ int main(void) {
   if (fork() == 0) {
     // int fd = open("/dev/tty.uart-console", 133250);
     int fd = open("/dev/tty.debug-console", O_RDWR | O_NOCTTY | O_SYNC | O_NONBLOCK);
+
     if (!(fd < 0)) {
       attribs(fd, 115200);
+
       block_attribs(fd, 0);
 
-      write(fd, "\r\n[serialsh]: initialized from the userland!\r\n", 46); // 0x2EuLL
+      write(fd, "\r\n[serialsh]: initialized from the userland!\r\n\r\n", 46); // 0x2EuLL
 
       pipe(pipe_shin);
+
       pipe(pipe_shout);
 
       dup2(pipe_shout[0], 0);
+
       dup2(spawn_shin, 1);
+
       dup2(spawn_shin, 2);
 
       kq = kqueue();
+
       assert(kq != -1);
+
       struct kevent ke;
-      char buf[0x400]; // 1024 bytes buffer
+
+      char buf[0x400];
+
       EV_SET(&ke, fd, EVFILT_READ, EV_ADD, 0, 5, NULL);
       kevent(kq, &ke, 1, NULL, 0, NULL);
+
       EV_SET(&ke, 0, EVFILT_READ, EV_ADD, 0, 5, NULL);
       kevent(kq, &ke, 1, NULL, 0, NULL);
+
       spawnproc();
 
       while (1) {
         EV_SET(&ke, 0, 0, 0, 0, 0, NULL);
+
         int uwu = kevent(kq, NULL, 0, &ke, 1, NULL);
-        if (uwu == 0) 
-          continue;
+
+        if (uwu == 0) continue;
+
         if (ke.ident == fd) {
           int rd = read(fd, buf, 0x400);
           write(1, buf, rd);
